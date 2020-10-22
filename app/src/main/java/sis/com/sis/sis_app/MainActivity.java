fe094ed4dc7a9cce69abaf5e6d5ad38ccec84758
tmaneesorn <70246867+tmaneesorn@users.noreply.github.com>
2020-10-22 16:54:48 +0700

    tmaneesorn-22-10-2020

---
+++ app/src/main/java/sis/com/sis/sis_app/MainActivity.java
@@ -0,0 +1,14 @@
+package sis.com.sis.sis_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
